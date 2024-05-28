9f34cab8f46b7174ec620c956d5acad110e61c46
gcmoura77 <gcmoura@gmail.com>
2024-05-28 17:12:50 -0300

    estudo de dados de outros anos lidos da API

--- app.py
+++ app.py
@@ -2,25 +2,47 @@ import streamlit as st
 from utils.filtro import get_monta_filtro
 from service.jogos import get_kpi
 
-# Set page title
-st.title('Estatísticas do São Paulo')
+# page configuration
+st.set_page_config(
+    page_title="Estatísticas do São Paulo",
+    page_icon="⚽", 
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-# Input field for stock symbol
-torneio = st.selectbox('Informe o torneio', ["Campeonato Paulista","Supercopa","Libertadores", "Brasileirão", "Copa do Brasil", "Todos"], index=5)
-tecnico = st.selectbox('Informe o técnico', ["Carpini","Milton Cruz","Zubeldia", "Todos"], index=3)
-local = st.multiselect('Informe o local do Jogo', ["Casa","Fora"],["Casa","Fora"])
+# create a sidebar
+with st.sidebar:
+    st.title('Estatísticas do São Paulo')
+    
+    # Input field for stock symbol
+    torneio = st.selectbox('Informe o torneio', ["Campeonato Paulista","Supercopa","Libertadores", "Brasileirão", "Copa do Brasil", "Todos"], index=5)
+    tecnico = st.selectbox('Informe o técnico', ["Carpini","Milton Cruz","Zubeldia", "Todos"], index=3)
+    local = st.multiselect('Informe o local do Jogo', ["Casa","Fora"],["Casa","Fora"])
 
 indicadores = get_kpi(get_monta_filtro(local,torneio,tecnico))
 
-# Display company name and current stock price in bold
-st.markdown(f'**Total de Jogos:** {indicadores["Total de Jogos"]}')
-st.markdown(f'**Pontos Possíveis:** {indicadores["Pontos possiveis"]}')
-st.markdown(f'**Pontos Ganhos:** {indicadores["Pontos Ganhos"]}')
-st.markdown(f'**Vitórias:** {indicadores["Vitórias"]}')
-st.markdown(f'**Empates:** {indicadores["Empates"]}')
-st.markdown(f'**Derrotas:** {indicadores["Derrotas"]}')
+#######################
+# Dashboard Main Panel
 
-# Closing prices chart
-st.subheader('Aproveitamento')
-st.metric(label="Aproveitamento", value=str(indicadores["Aproveitamento"])+'%')
+linha1 = st.columns(3, gap='medium')
+linha2 = st.columns(3, gap='medium')
+linha3 = st.columns(3, gap='medium')
+
+with linha1[0]:
+    st.metric(label="Total de Jogos", value=indicadores["Total de Jogos"])
+
+with linha1[1]:
+    st.metric(label="Pontos Ganhos", value=indicadores["Pontos Ganhos"])
+    
+with linha1[2]:
+    st.metric(label="Aproveitamento", value=str(indicadores["Aproveitamento"])+'%')
+
+with linha2[0]:
+    st.metric(label="Vitórias", value = indicadores["Vitórias"])
+
+with linha2[1]:
+    st.metric(label="Empates", value=indicadores["Empates"])
+
+with linha2[2]:        
+    st.metric(label="Derrotas", value=indicadores["Derrotas"])
+
 
